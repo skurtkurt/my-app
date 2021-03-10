fec8fceccbd0a179aedc079113597b0e6ef2bb38
skurtkurt <kpressly1@gmail.com>
2021-03-10 16:11:10 -0500

    create posts

--- src/App.js
+++ src/App.js
@@ -3,10 +3,45 @@ import './App.css';
 
 
 import { withAuthenticator,  } from 'aws-amplify-react';
+import { button } from 'aws-amplify';
+
+import { DataStore } from '@aws-amplify/datastore';
+import { Post } from './models';
+import { useEffect, useState } from 'react';
 
 function App() {
+const [post, setPost] = useState([]);
+
+useEffect(() => {
+  const func = async () => {
+    const models = await DataStore.query(Post);
+    setPost(models)
+  }
+
+  func()
+}, [])
+
+  const createPost = async () => {
+    const post = {
+      title: window.prompt('blog post title'),
+      content: window.prompt('blog post content')
+    }
+
+    const newPost = await DataStore.save(
+      new Post(post)
+    );
+  }
+
+
+
   return (
     <div className="App">
+      <button onClick={createPost}>Create a Post</button>
+      {post.map(post => <div key={post.id}>
+        <h1>{post.title}</h1>
+        <p>{post.content}</p>
+      </div>
+      )}
       
     </div>
   );
---
+++ src/models/schema.js
@@ -0,0 +1,137 @@
+export const schema = {
+    "models": {
+        "Comment": {
+            "name": "Comment",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "text": {
+                    "name": "text",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "author": {
+                    "name": "author",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "postID": {
+                    "name": "postID",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": false,
+                    "attributes": []
+                }
+            },
+            "syncable": true,
+            "pluralName": "Comments",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                },
+                {
+                    "type": "key",
+                    "properties": {
+                        "name": "byPost",
+                        "fields": [
+                            "postID"
+                        ]
+                    }
+                },
+                {
+                    "type": "auth",
+                    "properties": {
+                        "rules": [
+                            {
+                                "allow": "public",
+                                "operations": [
+                                    "create",
+                                    "update",
+                                    "delete",
+                                    "read"
+                                ]
+                            }
+                        ]
+                    }
+                }
+            ]
+        },
+        "Post": {
+            "name": "Post",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "title": {
+                    "name": "title",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "content": {
+                    "name": "content",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "Comments": {
+                    "name": "Comments",
+                    "isArray": true,
+                    "type": {
+                        "model": "Comment"
+                    },
+                    "isRequired": false,
+                    "attributes": [],
+                    "isArrayNullable": true,
+                    "association": {
+                        "connectionType": "HAS_MANY",
+                        "associatedWith": "postID"
+                    }
+                }
+            },
+            "syncable": true,
+            "pluralName": "Posts",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                },
+                {
+                    "type": "auth",
+                    "properties": {
+                        "rules": [
+                            {
+                                "allow": "public",
+                                "operations": [
+                                    "create",
+                                    "update",
+                                    "delete",
+                                    "read"
+                                ]
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    },
+    "enums": {},
+    "nonModels": {},
+    "version": "bb3106b6833dc6e94f9310e1e4e3c0c0"
+};
